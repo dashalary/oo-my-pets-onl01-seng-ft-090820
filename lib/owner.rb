c5c207ed23ff3b20f814309e1e2b34113db39dea
Dasha Lary <dk2800@nyu.edu>
2020-09-22 18:38:21 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,14 +1,17 @@
 class Owner
  attr_reader :name, :species, :say_species
-
+@@all = []
  
 def initialize(name)
 @name = name 
 @species = "human"
 @say_species = "I am a human."
+@@all << self
 end
 
-
+def self.all 
+  @@all
+end
  
  
  
