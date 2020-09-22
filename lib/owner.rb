b6d4693d30d3658a4f8197502dea08adcfe135a9
Dasha Lary <dk2800@nyu.edu>
2020-09-22 19:19:47 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -30,8 +30,8 @@ Dog.all.select {|dog| dog.owner == self}
 end
 
 def buy_cat(newcat)
-newcat = Cat.new(name, @owner)
- @owner.cats.all << newcat
+newcat = Cat.new(newcat, @owner)
+@owner.cats.all << newcat
 end
 
 
